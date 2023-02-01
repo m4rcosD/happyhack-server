a2c8ff9288cd46f29b4ac2c9a8d2602de295d906
marcos <mp.dias023@gmail.com>
2023-02-01 20:56:47 +0000

    finaly working

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const app = require("./app");
 const cors = require('cors')
 const MessageModel = require("./models/Message.models");
-app.use(cors());
 
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
-const PORT = process.env.PORT || 27017;
+const PORT = process.env.PORT || 5005;
 
 let myServer = app.listen(PORT, () => {
-  console.log(`Server listening on port http://localhost:${PORT}`);
+  console.log(`Server listening on port http://localhost:5005`);
 });
